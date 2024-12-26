6d463c7eedae1588a9659e0459ac2ecdf1b82066
diogo <diogocruzdc94@gmail.com>
2024-12-26 08:39:29 -0300

    using biblio useEffect to recovery data at useState

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { api } from "../services/api";
 
 const AuthContext = createContext({})
@@ -12,6 +12,10 @@ const AuthContext = createContext({})
                     const response = await api.post("/sessions", {email, password})
                     const {user, token} = response.data
 
+                    localStorage.setItem("@rocketnotes:user", JSON.stringify(user))/*local storage guardar dados no local storage JSON.stringfy converter em os dados em texto*/
+                    localStorage.setItem("@rocketnotes:token", token)
+
+
                    api.defaults.headers.authorization = `Bearer ${token}`
                    setData({user, token})
 
@@ -24,9 +28,25 @@ const AuthContext = createContext({})
                         alert("Não foi possível entrar")
                     }
                 }
+
+            }
+
+                useEffect(() => {
+                    const token = localStorage.getItem("@rocketnotes:token")
+                    const user = localStorage.getItem("@rocketnotes:user")
+
+                    if(token && user){
+                        api.defaults.headers.authorization = `Bearer ${token}`
+
+                        setData({
+                            token,
+                            user: JSON.parse(user)/**JSON.parse(voltar os dados  do usuario de texto par um objto json) */
+                        })
+                    }
+                }, [])/** useEffect serve para poder rebuscar as informações do local store, para depois retornar o estado atualisado, ela é uma arrow function one salvmos em um varial os token e o user, com a getItem("chave") */
                 
            
-            }
+            
             return(
 
                 <AuthContext.Provider value ={ {signIn, user: data.user }}>
