0ca8dbe6476cc0fbeede6a4b7b3123aa5a7faea4
diogo <diogocruzdc94@gmail.com>
2024-12-31 20:59:17 -0300

    alert on void inputs

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -6,18 +6,23 @@ import {Section} from '../../components/section'
 import {Button} from '../../components/button'
 import{Link} from 'react-router-dom'
 import{useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 import {Container, Form} from './styles'
+import { api } from '../../services/api'
 
 export function New(){
-    
+    const [title, setTitle] = useState("")
+    const [descriptions, setDescription]= useState("")
 
     const[links, setLinks]= useState([])
     const[newLink, setNewLink]= useState("")
 
     const[tags, setTags]= useState([])
     const[newTag, setNewTag]= useState("")
+
+    const navigate = useNavigate()
 
     function handleAddLink(){
         setLinks(prevState => [...prevState, newLink] )
@@ -27,7 +32,7 @@ export function New(){
     function handleRemoveLink(deleted){
         setLinks(prevState => prevState.filter(link => link !== deleted))
     }
-
+  
     function handleAddTag(){
         setTags(prevState =>[...prevState, newTag])/**prevState: prevenir os estados anteriores sem que precisa eliminar, e o "..." ele disfaz a lista anterior, despejando todas as informações numa nova lista */
         setNewTag("")
@@ -36,6 +41,29 @@ export function New(){
 
     function handleRemoveTag(deleted){
         setTags(prevState => prevState.filter(tag => tag !== deleted))
+    }
+
+    async function handleNewNote(){
+       if (!title) {
+        return alert('digite o titulo d NOTa')}
+
+       if (newLink) {
+
+           return alert('Tem um Link que não foi adicionado, adicione ou deixe vazio')
+        }
+      
+        if (newTag) {
+            return alert('Tem uma tag que não foi adicionada, adicione ou deixe vazio')
+           }
+       
+        await api.post("/notes", {
+            title,
+            descriptions,
+            tags,
+            links
+        })
+        alert("Nota criada com sucesso")
+        navigate("/")
     }
 
     return(
@@ -49,8 +77,15 @@ export function New(){
                         <Link to="/">Voltar</Link>
                     </header>
 
-                    <Input placeholder='Título' />
-                    <Textarea placeholder='Observações'/>
+                    <Input
+                     placeholder='Título' 
+                     onChange= {e=> setTitle(e.target.value)}
+
+                     />
+                    <Textarea 
+                    placeholder='Observações'
+                    onChange = {e=> setDescription(e.target.value)}
+                    />
 
                     <Section title='Links úteis'>
                         
@@ -99,7 +134,10 @@ export function New(){
                     </div>
                     </Section>
 
-                    <Button title='Salvar'/>
+                    <Button
+                     title='Salvar' 
+                     onClick={handleNewNote}
+                     />
                 </Form>
             </main>
         </Container>
